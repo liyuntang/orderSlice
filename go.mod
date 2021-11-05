9d2335262ff5a61532430931ef4ba604ee4d3bd3
liyuntang <liyuntang@liyuntang.local>
2021-11-05 14:51:46 +0800

    add

---
+++ go.mod
@@ -0,0 +1,8 @@
+module orderSlice
+
+go 1.13
+
+require (
+	gorm.io/driver/mysql v1.1.3
+	gorm.io/gorm v1.22.2
+)
--- main.go
+++ main.go
@@ -1,6 +1,22 @@
 package orderSlice
 
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
 func GetSlice () []string {
 	return []string{"aaa", "bbb", "ccc"}
 	
+}
+
+func CreateDb() {
+	dsn := "admin_m:XphoeGz6NED4yQMQ0yJc@tcp(10.10.10.197:9000)/db_platform?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("connect to mysql is bad, err is", err)
+		return
+	}
+	fmt.Println("connect to mysql is ok")
 }
