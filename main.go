4dc6ea5c3a6e1ce90d5c51ff80e9eab80ea19718
驭风者 <liyuntang19891218@163.com>
2021-11-05 15:35:49 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,22 +1,22 @@
 package orderSlice
 
-import (
-	"fmt"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-)
+// import (
+// 	"fmt"
+// 	"gorm.io/driver/mysql"
+// 	"gorm.io/gorm"
+// )
 
 func GetSlice () []string {
 	return []string{"aaa", "bbb", "ccc"}
 	
 }
 
-func CreateDb() {
-	dsn := "mmmmmmm:XphJc@tcp(17.140.100.111:9000)/db_platform?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		fmt.Println("connect to mysql is bad, err is", err)
-		return
-	}
-	fmt.Println("connect to mysql is ok")
-}
+// func CreateDb() {
+// 	dsn := "mmmmmmm:XphJc@tcp(17.140.100.111:9000)/db_platform?charset=utf8mb4&parseTime=True&loc=Local"
+// 	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// 	if err != nil {
+// 		fmt.Println("connect to mysql is bad, err is", err)
+// 		return
+// 	}
+// 	fmt.Println("connect to mysql is ok")
+// }
