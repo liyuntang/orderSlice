28a41523cc586688f2118173c2a5476ffb5c4414
liyuntang <liyuntang@liyuntang.local>
2021-11-05 15:08:14 +0800

    sdfsdf

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func GetSlice () []string {
 }
 
 func CreateDb() {
-	dsn := "admin_m:XphoeGz6NED4yQMQ0yJc@tcp(10.10.10.197:9000)/db_platform?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "mmmmmmm:XphJc@tcp(17.140.100.111:9000)/db_platform?charset=utf8mb4&parseTime=True&loc=Local"
 	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println("connect to mysql is bad, err is", err)
